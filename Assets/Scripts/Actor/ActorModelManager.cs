af14cf5afd19ae9470c162ffb28bab5ffd735a19
Kyle Misner <ismakefire@gmail.com>
2018-06-08 11:02:46 -0700

    New singleton for housing actors and parent behavior for collecting them. -Scene changes: added component for parent actor script.

---
+++ Assets/Scripts/Actor/ActorModelManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Misner.PalmRTS.Actor
+{
+	public class ActorModelManager
+	{
+        #region Singleton
+
+        private static ActorModelManager _instance = null;
+
+        public static ActorModelManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ActorModelManager();
+                }
+
+                return _instance;
+            }
+        }
+
+        private ActorModelManager()
+        {
+        }
+
+        #endregion
+
+        #region Variables
+
+        private List<ActorBehavior> _actorBehaviors = new List<ActorBehavior>();
+
+        #endregion
+
+        #region Public Interface
+
+		public void Add(ActorBehavior actorBehavior)
+        {
+            _actorBehaviors.Add(actorBehavior);
+
+            Debug.LogFormat("{0}.Add(), _actorBehaviors.Count = {1}", this.ToString(), _actorBehaviors.Count);
+        }
+
+        #endregion
+	}
+}
---
+++ Assets/Scripts/Actor/ActorParentBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Misner.PalmRTS.Util;
+using UnityEngine;
+
+namespace Misner.PalmRTS.Actor
+{
+	public class ActorParentBehavior : MonoBehaviour
+    {
+        #region MonoBehaviour
+
+        // Use this for initialization
+        protected void Start()
+        {
+            ActorBehavior[] existingBehaviors = transform.GetComponentsInChildren<ActorBehavior>();
+
+            if (!existingBehaviors.IsNullOrEmpty())
+            {
+                Debug.LogFormat("{0}.Start(), existing tiles found! existingBehaviors.Length = {1}", this.ToString(), existingBehaviors.Length);
+
+                foreach (ActorBehavior childBehavior in existingBehaviors)
+                {
+                    ActorModelManager.Instance.Add(childBehavior);
+                }
+            }
+        }
+
+        // Update is called once per frame
+        protected void Update()
+        {
+        }
+
+        #endregion
+	}
+}
