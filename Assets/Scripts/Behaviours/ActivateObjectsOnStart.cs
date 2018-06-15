88606636db0ac5ad40a457682fec637cd1448546
Kyle Misner <ismakefire@gmail.com>
2018-06-15 13:29:05 -0700

    Utility behavior for keeping the right objects active on start. Speeding up workflow.

---
+++ Assets/Scripts/Behaviours/ActivateObjectsOnStart.cs
@@ -0,0 +1,31 @@
+﻿using Misner.PalmRTS.Util;
+using UnityEngine;
+
+namespace Misner.PalmRTS.Behaviour
+{
+	public class ActivateObjectsOnStart : MonoBehaviour
+	{
+        #region SerializeField
+
+        [SerializeField]
+        private GameObject[] _activationTargets;
+
+        #endregion
+
+        #region MonoBehaviour
+
+		// Use this for initialization
+		protected void Start ()
+		{
+            if (!_activationTargets.IsNullOrEmpty())
+            {
+                foreach (GameObject target in _activationTargets)
+                {
+                    target.SetActive(true);
+                }
+            }
+        }
+
+        #endregion
+	}
+}
