cccf7ccc3f0aa3ead763595376295bdf83fdf0c8
Kyle Misner <ismakefire@gmail.com>
2018-07-10 11:51:22 -0700

    New drill behavior.

---
+++ Assets/Scripts/Player/DrillStructureBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Misner.PalmRTS.Actor;
+using UnityEngine;
+
+namespace Misner.PalmRTS.Player
+{
+    [RequireComponent(typeof(ActorBehavior))]
+	public class DrillStructureBehavior : MonoBehaviour
+	{
+        #region MonoBehaviour
+
+		// Use this for initialization
+		protected void Start ()
+		{
+		}
+
+		// Update is called once per frame
+		protected void Update ()
+		{
+		}
+
+        #endregion
+	}
+}
